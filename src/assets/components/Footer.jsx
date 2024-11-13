ad86a2a002dd84874bad5900f5f9c01e11b29ca7
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-11-14 01:34:15 +0530

    increse font weight

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
         </div>
         <div className="w-fit mb-8 md:mb-0 ">
           <nav className="text-textSecondary011 tracking-wide text-lg font-stylish ">
-            <ul className="flex flex-col w-full text-nowrap justify-center md:justify-end  md:flex-row space-y-4 md:space-y-0 md:space-x-8">
+            <ul className="flex flex-col w-full font-stylishBold text-nowrap justify-center md:justify-end  md:flex-row space-y-4 md:space-y-0 md:space-x-8">
               <li className="cursor-pointer">Home</li>
               <li className="cursor-pointer w-fit">About Us</li>
               <li className="cursor-pointer w-fit">Services</li>
@@ -61,7 +61,7 @@ const Footer = () => {
         <div className="w-full md:w-1/2 h-full md:space-y-8 ">
           <div className="w-full flex justify-start">
             <div className="mb-0">
-              <h2 className="text-textPrimary011 font-stylish mb-4">
+              <h2 className="text-textPrimary011 font-stylishBold mb-4">
                 Contact Us
               </h2>
               <p className="text-textSecondary011">+1 (999) 888-77-66</p>
@@ -70,7 +70,7 @@ const Footer = () => {
           </div>
           <div className="w-full flex justify-between">
             <div className="m-0">
-              <h2 className="text-textPrimary011 font-stylish mb-4">
+              <h2 className="text-textPrimary011 font-stylishBold mb-4 ">
                 Location
               </h2>
               <p className="text-textSecondary011">
@@ -80,7 +80,7 @@ const Footer = () => {
             </div>
 
             <div className="flex flex-col justify-between">
-              <h2 className="text-textPrimary011 font-stylish mb-4">
+              <h2 className="text-textPrimary011 font-stylishBold mb-4">
                 Languages
               </h2>
               <div className="flex space-x-2 text-textSecondary011">
