1e3d1efff40caf8d404edbb5475e2faae34f7e4f
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-11-14 02:25:58 +0530

    hover text animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import AboutUsSection from "./assets/components/AboutUsSection";
+import BigText from "./assets/components/BigText";
 import Footer from "./assets/components/Footer";
 
 function App() {
   return (
     <>
       <AboutUsSection />
+      <BigText />
       <Footer />
     </>
   );
---
+++ src/assets/components/BigText.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TextHoverEffect from "./TextHoverEffect";
+
+export default function BigText() {
+  return (
+    <div className="flex justify-center items-center h-fit bg-black">
+      <TextHoverEffect text="Artisan" />
+    </div>
+  );
+}
